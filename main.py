d22713e2556224da973b615b222000ff858418ff
Deadlykeyboard <niek.van.reenen@xs4all.nl>
2023-10-08 19:31:31 +0200

    tiny thingies

--- main.py
+++ main.py
@@ -2,22 +2,24 @@
 import time
 import os
 
-
-
 print("FETCHING DATA...")
+
 for i in range(10):
     if i == 1:
         from SmartSunPos import SmartSunPos
     if i != 1:
         time.sleep(.1)
+        # later loading here under more 'ifs' for now sleep.
     print(f"[LOADING] {'##' * i}\033[1A")
+
 print()
 
 if __name__ == "__main__":
     location = (52.09061, 5.12143)
+    timezone = 2
     while True:
-        termsize = os.get_terminal_size().columns - 1
-        obj = SmartSunPos(return_time=True, location=location, timezone=2, refraction=True)
+        termsize = os.get_terminal_size().columns
+        obj = SmartSunPos(return_time=True, location=location, timezone=timezone, refraction=True)
         azimuth, elevation, time_of_measurement = obj.sun_position
         print("-" * termsize)
         print(f"Last measurement: {time_of_measurement}")
